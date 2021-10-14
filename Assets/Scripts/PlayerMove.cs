7759e85fc1af2a6526a3440e9f000f93acaec35b
Vmorais22 <45717130+Vmorais22@users.noreply.github.com>
2021-10-14 13:42:43 +0200

    Deco and move

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed = 3.5f;
+
+    private float gravity = 10f;
+
+    private CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        Camera.main.transform.position = new Vector3(-0.4854f, 0.7f, 4.26811f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0, vertical);
+        Vector3 velocity = direction * speed;
+        velocity = Camera.main.transform.TransformDirection(velocity);
+        velocity.y -= gravity;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
