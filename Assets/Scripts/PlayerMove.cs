5d1fbd5f8679e95a63db44d7d74db18a0f03b61c
Vmorais22 <45717130+Vmorais22@users.noreply.github.com>
2021-10-17 16:26:04 +0200

    Animaciones armarios solo abrir

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,26 +4,27 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    //MOVIMIENTO
     public float speed = 3.5f;
+    private float gravity = 10f;
+    private CharacterController controller;
+
+    //SELECCIÓN
     public float distanceOfRaycast = 0.000000000005f;
     public GameObject myHand;
-
-    private float gravity = 10f;
+    private Transform padrecito;
     private RaycastHit _hit;
     private bool selected = false;
 
-    private CharacterController controller;
-    private Transform padrecito;
 
-    bool debugEnCuenta = false;
-
+    //DISTINGUIR INTERACCIÓ
     public Material cogido;
+    public Material agarrado;
+    private Material initMaterial;
 
     void Start()
     {
         controller = GetComponent<CharacterController>();
-     
-
     }
 
     // Update is called once per frame
@@ -35,35 +36,64 @@ public class PlayerMove : MonoBehaviour
         if(Physics.Raycast(ray, out _hit, distanceOfRaycast))
         {
             dist = Vector3.Distance(_hit.transform.position, transform.position);
-            if (Input.GetButtonDown("Fire1") && _hit.transform.CompareTag("Cogido") && dist < 3f)
+            if (dist < 2f) //si el objeto esta al alcance
             {
-                //_hit.transform.GetComponent<Renderer>().material = cogido;
+                if (_hit.transform.CompareTag("Cogido")) //si el objeto es cogible
+                {
+                    /*initMaterial = _hit.transform.GetComponent<Renderer>().material; //guarda material inicial
+                    _hit.transform.GetComponent<Renderer>().material = cogido; //pintalo amarillo*/
+                    Debug.Log("cogible");
 
-                if(!selected)
-                {
-                    Debug.Log("lo cojo");
-                    padrecito = _hit.transform.parent.gameObject.transform;
-                    _hit.transform.SetParent(myHand.transform);
-                    _hit.collider.isTrigger = true;
-                    _hit.rigidbody.useGravity = false;
-                    _hit.transform.localPosition = new Vector3(0f, 0f, 0f);
-                    selected = true;
+                    if (Input.GetButtonDown("Fire1")) //si apreto y...
+                    {
+                        if (!selected) //no esta seleccionado lo cojo
+                        {
+                            Debug.Log("lo cojo");
+                            padrecito = _hit.transform.parent.gameObject.transform;
+                            _hit.transform.SetParent(myHand.transform);
+                            _hit.collider.isTrigger = true;
+                            _hit.rigidbody.useGravity = false;
+                            _hit.transform.localPosition = new Vector3(0f, 0f, 0f);
+                            selected = true;
+                        }
+                        else if (selected)//esta seleccionado lo suelto
+                        {
+                            Debug.Log("lo suelto");
+                            _hit.transform.SetParent(padrecito);
+                            _hit.collider.isTrigger = false;
+                            _hit.rigidbody.useGravity = true;
+                            selected = false;
+                        }
+                    }
                 }
-                else if (selected)
+
+                if (_hit.transform.CompareTag("ArmAbIzDer"))
                 {
-                    Debug.Log("lo suelto");
-                    _hit.transform.SetParent(padrecito);
-                    _hit.collider.isTrigger = false;
-                    _hit.rigidbody.useGravity = true;
-                    selected = false;
+                    if (Input.GetButtonDown("Fire1")) //si apreto y...
+                    {
+                        _hit.transform.GetComponent<Animation>().Play("OpenLeftRight");
+                        _hit.collider.isTrigger = true;
+                        _hit.transform.gameObject.tag = "ArmCiDerIz";
+                    }
                 }
+                if (_hit.transform.CompareTag("ArmAbDerIz"))
+                {
+                    if (Input.GetButtonDown("Fire1")) //si apreto y...
+                    {
+                        _hit.transform.GetComponent<Animation>().Play("OpenRightLeft");
+                        _hit.collider.isTrigger = true;
+                        _hit.transform.gameObject.tag = "ArmCiIzDer";
+                    }
+                }
+
 
             }
-            else
+            
+            else //si no estoy a distancia
             {
-                if (dist > 2) Debug.Log("muy lejos");
+                
                 if (!_hit.transform.CompareTag("Cogido") && dist < 2f) Debug.Log("no es Cogible");
-                if (_hit.transform.CompareTag("Cogido") && dist < 2f) Debug.Log("se puede coger");
+                if (dist > 2) Debug.Log("muy lejos");
             }
             
            
